ff7c8ec1e1eb7af0afd27aca0f23b4cae9739050
Divyansh-Kaushik84 <84divyanshkaushik@gmail.com>
2024-09-12 00:43:29 +0530

    Initial Commit

---
+++ groceryMart/groceryMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  groceryMartApp.swift
+//  groceryMart
+//
+//  Created by Divyansh Kaushik on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct groceryMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
